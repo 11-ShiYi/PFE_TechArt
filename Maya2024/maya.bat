5abcb7144f5b6d9c80c490b87ccda7100e7d48ef
ON1E1 <33352771+11-ShiYi@users.noreply.github.com>
2025-05-09 15:22:30 +0200

    Initial commit

---
+++ Maya2024/maya.bat
@@ -0,0 +1,12 @@
+@echo off
+set MAYA_PATH="C:\Program Files\Autodesk\Maya2024\bin\maya.exe"
+
+:: add env variable
+set MAYA_SCRIPT_PATH=%~dp0scripts;%MAYA_SCRIPT_PATH%
+set PYTHONPATH=%~dp0scripts;%PYTHONPATH%
+set XBMLANGPATH=%XBMLANGPATH%;%~dp0prefs\icon
+
+
+
+start "" %MAYA_PATH%
+exit
---
+++ SubstancePainter/SubstancePainter.bat
@@ -0,0 +1,13 @@
+@echo off
+set SUBSTANDEPAINTER_PATH="D:\Software\Adobe Substance 3D Painter\Adobe Substance 3D Painter.exe"
+set "SHELF_PATH=%~dp0shelf"
+
+:: add env variable
+set OCIO=%~dp0OCIO\simple.config.ocio
+
+set "SOURCE_PATH=%~dp0export-presets"
+set "TARGET_PATH=%USERPROFILE%\Documents\Adobe\Adobe Substance 3D Painter\assets\export-presets"
+xcopy "%SOURCE_PATH%\*" "%TARGET_PATH%\" /E /H /Y
+
+start "" %SUBSTANDEPAINTER_PATH%
+exit
