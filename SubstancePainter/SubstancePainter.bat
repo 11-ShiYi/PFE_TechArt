c53ce97fe5cca721e31a3a73eae289b0c25937a1
ON1E1 <33352771+11-ShiYi@users.noreply.github.com>
2025-10-29 16:25:05 +0100

    fix grammar error

--- SubstancePainter/SubstancePainter.bat
+++ SubstancePainter/SubstancePainter.bat
@@ -8,9 +8,10 @@ set OCIO=%~dp0OCIO\simple.config.ocio
 set "SOURCE_PATH=%~dp0export-presets"
 set "TARGET_PATH=%USERPROFILE%\Documents\Adobe\Adobe Substance 3D Painter\assets\export-presets"
 
-set "SOURCE_SMART_MATERIAL"=%~dp0smart-materials"
-set "TARGET_SMART_MATERIAL"=%USERPROFILE%\Documents\Adobe\Adobe Substance 3D Painter\assets\smart-materials"
+set "SOURCE_SMART_MATERIAL=%~dp0smart-materials"
+set "TARGET_SMART_MATERIAL=%USERPROFILE%\Documents\Adobe\Adobe Substance 3D Painter\assets\smart-materials"
 xcopy "%SOURCE_PATH%\*" "%TARGET_PATH%\" /E /H /Y
+xcopy "%SOURCE_SMART_MATERIAL%\*" "%TARGET_SMART_MATERIAL%\" /E /H /Y
 
 start "" %SUBSTANDEPAINTER_PATH%
 exit
