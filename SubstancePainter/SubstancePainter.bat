cf7d545232de870a6d7c7bc30d10f90c22c61a92
ON1E1 <33352771+11-ShiYi@users.noreply.github.com>
2025-10-29 15:56:48 +0100

    add new tool to convert file

--- SubstancePainter/SubstancePainter.bat
+++ SubstancePainter/SubstancePainter.bat
@@ -7,6 +7,9 @@ set OCIO=%~dp0OCIO\simple.config.ocio
 
 set "SOURCE_PATH=%~dp0export-presets"
 set "TARGET_PATH=%USERPROFILE%\Documents\Adobe\Adobe Substance 3D Painter\assets\export-presets"
+
+set "SOURCE_SMART_MATERIAL"=%~dp0smart-materials"
+set "TARGET_SMART_MATERIAL"=%USERPROFILE%\Documents\Adobe\Adobe Substance 3D Painter\assets\smart-materials"
 xcopy "%SOURCE_PATH%\*" "%TARGET_PATH%\" /E /H /Y
 
 start "" %SUBSTANDEPAINTER_PATH%
